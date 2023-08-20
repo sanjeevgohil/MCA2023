60529d6c3aae89c7c0ca5b0f4b0172fa7f418902
Sanjeev Gohil <118007831+sanjeevgohil@users.noreply.github.com>
2023-08-20 11:25:58 +0530

    Add files via upload

---
+++ JAVA/J2.java
@@ -0,0 +1,9 @@
+//write an program to input first and last name using command line
+class J2
+{
+	public static void main(String args[])
+	{	
+		System.out.println("First Name is:"+args[0]);
+		System.out.println("Last Name is:"+args[1]);
+	}
+}
