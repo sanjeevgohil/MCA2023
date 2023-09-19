960e8fd40357c2de9dc1c3c57f1de105e67ce04d
Sanjeev Gohil <118007831+sanjeevgohil@users.noreply.github.com>
2023-09-19 07:46:28 +0530

    Add files via upload

---
+++ JAVA/J47.java
@@ -0,0 +1,29 @@
+//write a program of Multilevel Inheritance
+class A
+{
+	void dispA(){
+		System.out.println("Class A");
+	}
+}
+class B extends A
+{
+	void dispB(){
+		System.out.println("Class B");
+	}
+}
+class C extends B
+{
+	void dispC(){
+		System.out.println("Class C");
+	}
+}
+class J47
+{
+	public static void main(String args[])
+	{
+		C c1 = new C();
+		c1.dispA();
+		c1.dispB();
+		c1.dispC();
+	}
+}
