dde44bead102bd6bbdb24e045e31ad653dd3f0d0
Sanjeev Gohil <118007831+sanjeevgohil@users.noreply.github.com>
2023-12-08 12:26:39 +0530

    Delete JAVA directory

--- JAVA/J2.java
+++
@@ -1,9 +0,0 @@
-//write an program to input first and last name using command line
-class J2
-{
-	public static void main(String args[])
-	{	
-		System.out.println("First Name is:"+args[0]);
-		System.out.println("Last Name is:"+args[1]);
-	}
-}
--- JAVA/J58.java
+++
@@ -1,8 +0,0 @@
-import static java.lang.System.*;
-class J58
-{
-    public static void main(String args[])
-    {
-        out.println("Wel-Come");
-    }
-}
